1020f0f4bd8e2d790d04a223f4093a1171665322
ApelsinDev <nik_zhur_serg@mail.ru>
2023-05-02 01:26:27 +0400

    Init commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.myapplication
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bNav.selectedItemId = R.id.item1
+        binding.bNav.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.item1 -> {
+                    Toast.makeText(this, "Главная", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "Поиск", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "Анализ", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item4 -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item5 -> {
+                    Toast.makeText(this, "Профиль", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+        Log.d("MyLogMAct", "OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MyLogMAct", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MyLogMAct", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MyLogMAct", "onPause")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MyLogMAct", "onDestroy")
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MyLogMAct", "onStop")
+    }
+
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MyLogMAct", "onRestart")
+    }
+}
